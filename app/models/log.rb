269ab930d20e70b2d2414c184f224d247ac4d48a
Alessandra Caroline Faisst <caroline.faisst@gmail.com>
2015-09-24 17:39:09 -0700

    add methods to calculate DD coordinates

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,4 +1,28 @@
 class Log < ActiveRecord::Base
   belongs_to :trip
 
+  def latitude
+    if self.latitude_hemisphere == "N"
+      decimal_degree(self.latitude_degrees, self.latitude_minutes, self.latitude_seconds)
+    else
+      - decimal_degree(self.latitude_degrees, self.latitude_minutes, self.latitude_seconds)
+    end
+  end
+
+  def longitude
+    if self.longitude_hemisphere == "E"
+      decimal_degree(self.longitude_degrees, self.longitude_minutes, self.longitude_seconds)
+    else
+      - decimal_degree(self.longitude_degrees, self.longitude_minutes, self.longitude_seconds)
+    end
+  end
+
+  def decimal_degree(degrees, minutes, seconds)
+    (degrees.to_f + decimal(minutes, seconds)).to_f
+  end
+
+  def decimal(minutes, seconds)
+    ((minutes.to_f/60) + (seconds.to_f/(60*60))).to_f
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 sardinia = Trip.new(title: "Sardinia", skipper_name: "Jannis", crew_name: "Spitzencrew", boat_name: "Sausebrause", boat_type: "Bavaria50", start_date: "2015-05-10", end_date: "2015-05-22", origin: "Porto Cervo", destination: "Olbia")
 sardinia.save
 
-sardinia.log = Log.create(nautical_mile: 1, wind_knots: 7, current_speed: 0, weather: "sunny", incidents: "Scott fell off the boat", latitude_hemisphere: "N", latitude_degrees: 41, latitude_minutes: 10, latitude_seconds: 33.3, longitude_hemisphere: "E", longitude_degrees: 9, longitude_minutes: 35, longitude_seconds: 43.2)
+sardinia.logs << Log.create(nautical_mile: 1, wind_knots: 7, current_speed: 0, weather: "sunny", incidents: "Scott fell off the boat", latitude_hemisphere: "N", latitude_degrees: 41, latitude_minutes: 10, latitude_seconds: 33.3, longitude_hemisphere: "E", longitude_degrees: 9, longitude_minutes: 35, longitude_seconds: 43.2)
